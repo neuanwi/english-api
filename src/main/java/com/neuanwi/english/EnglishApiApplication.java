aa1940196ae8728896dac7a73360d7440d0b2eef
neuanwi <windhkhj1@gmail.com>
2026-02-19 17:28:33 +0900

    prototype

---
+++ src/main/java/com/neuanwi/english/EnglishApiApplication.java
@@ -0,0 +1,13 @@
+package com.neuanwi.english;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishApiApplication.class, args);
+	}
+
+}
